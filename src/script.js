ade9841ab6d4b0f4e8120343d6b717de24be8a0e
Charles <charlesnwoye2@gmail.com>
2023-02-18 10:54:47 +0100

    added extract contact form info feature

--- src/script.js
+++ src/script.js
@@ -1,4 +1,13 @@
+// nav
 const menu = document.querySelector('.mobile-menu');
 const icon = document.querySelector('.mobile-icon');
 
 icon.addEventListener('click', () => menu.classList.toggle('hidden'));
+
+// contact details
+const sendEmail = () => {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+  console.log(name, email, message);
+};
