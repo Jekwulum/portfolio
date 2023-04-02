daa564eddf73607673c9b4a855f2a721532d1725
Charles <charlesnwoye2@gmail.com>
2023-04-02 18:17:09 +0100

    added project page

--- src/script.js
+++ src/script.js
@@ -2,4 +2,24 @@
 const menu = document.querySelector('.mobile-menu');
 const icon = document.querySelector('.mobile-icon');
 
-icon.addEventListener('click', () => menu.classList.toggle('hidden'));
+icon.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+});
+
+function showText() {
+    document.querySelector('.text-div').classList.remove('md:hidden');
+}
+
+function hideText() {
+    document.querySelector('.text-div').classList.add('md:hidden');
+}
+
+function showSlideInDiv() {
+    const slideInDiv = document.querySelector('.slide-in-div');
+    slideInDiv.classList.remove('translate-x-full');
+}
+
+function showSlideOutDiv() {
+    const slideInDiv = document.querySelector('.slide-in-div');
+    slideInDiv.classList.add('translate-x-full');
+}
